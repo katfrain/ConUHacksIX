f2a2059a972b83ba4074698ea0edc9e791e698cc
Ethan Logie <170824744+EthanLogie@users.noreply.github.com>
2025-02-01 17:05:11 -0500

    Added the ui for posts, and the ability to scroll

---
+++ Screens/HomeScreen.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FlatList, SafeAreaView, StyleSheet, View, Text } from "react-native";
+import ItemCardTest from "../components/itemCartTest";
+import { ITEMS } from "../data-temp/items.jsx";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={ITEMS}
+        keyExtractor = {(item) => item.id.toString()}
+        renderItem = {({ item }) => <ItemCardTest item={item} />}
+        numColumns={2} //Displays 2 collumns
+        columnWrapperStyle={styles.columnWrapper}
+        contentContainerStyle = {{
+            paddingBottom: 50,
+            flexGrow: 1
+        }} 
+        showsVerticalScrollIndicator ={false}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f8f8",
+  },
+  list:{
+    paddingBottom: 50
+  },
+  columnWrapper:{
+    justifyContent: "space-between",
+    paddingHorizontal: 10
+  }
+});
+
+export default HomeScreen;
---
+++ data-temp/items.jsx
@@ -0,0 +1,52 @@
+// data/items.jsx
+
+export const ITEMS  = [
+{
+    id: 1,
+    title: "Bicycle Blue with a broken wheel",
+    photo: require("../assets/images/bike.png"),
+    type: "trade"
+},
+{
+    id: 2,
+    title: "Hat",
+    photo: require("../assets/images/hat.png"),
+    type: "free"
+},
+{
+    id: 3,
+    title: "Textbook",
+    photo: require("../assets/images/book.png"),
+    type: "free"
+},
+{
+    id: 4,
+    title: "Couch",
+    photo: require("../assets/images/couch.png"),
+    type: "trade"
+},
+{
+    id: 5,
+    title: "Textbook",
+    photo: require("../assets/images/react-logo.png"),
+    type: "trade"
+},
+{
+    id: 6,
+    title: "Textbook",
+    photo: require("../assets/images/react-logo.png"),
+    type: "trade"
+},
+{
+    id: 7,
+    title: "Textbook",
+    photo: require("../assets/images/react-logo.png"),
+    type: "trade"
+},
+{
+    id: 8,
+    title: "Textbook",
+    photo: require("../assets/images/react-logo.png"),
+    type: "trade"
+}
+]
