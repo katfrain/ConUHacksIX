be59e3ebf273eed2dc18e7708fcc7a3f9f7ea497
Camila Kin Márquez Sosa <camilakinmarquezsosa@camis-laptop.wireless.concordia.ca>
2025-02-01 15:28:17 -0500

    added login user creation and authentication

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,51 @@
-import { Text, View } from "react-native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer, NavigationIndependentTree} from "@react-navigation/native";
+import tempHome from "@/app/screens/tempHome"
+import Login from "@/app/screens/Login";
+import {useEffect, useState} from "react";
+import {onAuthStateChanged, User} from "@firebase/auth";
+import {FIREBASE_AUTH} from "@/Configurations/FirebaseConfig";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+
+
+const Stack = createNativeStackNavigator()
+
+// ------------------------ this is just for testing after login
+// its equivalent (navigation) can, and should be, moved to a different file in our navigation folder later on
+const InsideStack = createNativeStackNavigator()
+
+function InsideLayout() {
+    return(
+        <InsideStack.Navigator>
+            <InsideStack.Screen name="tempHome" component={tempHome} />
+        </InsideStack.Navigator>
+    )
+}
+// ------------------------
+
+
+export default function App() {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        onAuthStateChanged(FIREBASE_AUTH, (user) => {
+            console.log('user', user);
+            setUser(user);
+        });
+    }, []);
+
+    return (
+        <NavigationIndependentTree>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="LogIn">
+                    {user ? (
+                        <Stack.Screen name={"Home"} component={InsideLayout} options={{headerShown:false}} />
+                        ) : (
+                        <Stack.Screen name={"LogIn"} component={Login} />
+                    )}
+
+                </Stack.Navigator>
+            </NavigationContainer>
+        </NavigationIndependentTree>
+    )
 }
